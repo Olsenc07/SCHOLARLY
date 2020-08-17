fd4c531b05a523081a2f62b8d77bdfdf3846af86
Chase Olsen <skalolsen@gmail.com>
2020-08-16 22:35:40 -0400

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+import { observable } from 'rxjs'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
