2312210c87fd0e1990a461b28e96f43e37b91c6e
Chase Olsen <skalolsen@gmail.com>
2020-09-05 21:32:31 -0400

    created and worked on terms & conditions html page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { TermsComponent } from './terms/terms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule,
