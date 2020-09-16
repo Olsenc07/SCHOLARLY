83597d4a92b01a103ae5b09d12186862bd23eaee
Chase Olsen <skalolsen@gmail.com>
2020-09-15 21:00:14 -0400

    fixed postpage bug for now

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-page.component.scss'],
 })
 export class PostPageComponent implements OnInit {
-  public submitAttempt: boolean = false;
+  public submitAttempt = false;
   constructor() {}
 
   ngOnInit(): void {}
