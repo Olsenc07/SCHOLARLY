2555ac864f2c5793e10108eb927eb2fcb6b1d683
Chase Olsen <skalolsen@gmail.com>
2020-09-23 00:05:41 -0400

    trying to add the input field to the password part in signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -3,14 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
   MatIconModule;
-  
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
+// export class input-box {
+//   value = 'Clear me';
+// }
