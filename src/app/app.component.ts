a771f9cb13c71c796e834b54763d1486b7e1b1da
Chase Olsen <skalolsen@gmail.com>
2020-10-24 01:54:24 -0400

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
-@Component({ 
-  selector: 'app-root',
+@Component({
+  selector: 'app-home-page',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-SCHOLARLY';
