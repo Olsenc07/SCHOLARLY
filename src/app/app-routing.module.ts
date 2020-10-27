8cceed353ab8c001cc204baa2f3d0ff292256ddf
Chase Olsen <skalolsen@gmail.com>
2020-10-26 23:41:35 -0400

    :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { FriendsActivityComponent } from './friends-activity/friends-activity.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RetrievePasswordComponent } from './retrieve-password/retrieve-password.component';
@@ -23,8 +24,8 @@ const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'retrieve-password', component: RetrievePasswordComponent },
   { path: 'search', component: SearchComponent },
-  {path: 'edit-profile', component: EditProfileComponent},
-  {path: 'friends-activity', component:}
+  { path: 'edit-profile', component: EditProfileComponent },
+  { path: 'friends-activity', component: FriendsActivityComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
--- src/app/retrieve-password/retrieve-password.component.ts
+++ src/app/retrieve-password/retrieve-password.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-retrieve-password',
   templateUrl: './retrieve-password.component.html',
-  styleUrls: ['./retrieve-password.component.scss']
+  styleUrls: ['./retrieve-password.component.scss'],
 })
 export class RetrievePasswordComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    // TODO: wire up to login request
+    console.log(this.loginForm.value);
   }
-
 }
