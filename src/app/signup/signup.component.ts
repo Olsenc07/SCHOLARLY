843ce8ec6e1a7bd8b2c8aa020d957c400ade17ff
Chase Olsen <skalolsen@gmail.com>
2020-11-06 20:11:26 -0500

    all clears done

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -8,21 +8,57 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
   MatIconModule;
+
+  username: FormControl = new FormControl('');
+  password: FormControl = new FormControl('');
+  major: FormControl = new FormControl('');
+  sport: FormControl = new FormControl('');
+  name: FormControl = new FormControl('');
+  birthday: FormControl = new FormControl('');
+  gender: FormControl = new FormControl('');
+  relationship: FormControl = new FormControl('');
+
   signupForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-    major: new FormControl(''),
-    sport: new FormControl(''),
-    name: new FormControl(''),
-    birthday: new FormControl(''),
-    gender: new FormControl(''),
-    relationship: new FormControl(''),
+    username: this.username,
+    password: this.password,
+    major: this.major,
+    sport: this.sport,
+    name: this.name,
+    gender: this.gender,
+    relationship: this.relationship,
   });
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  clearUsername(): void {
+    this.username.setValue('');
+  }
+
+  clearPassword(): void {
+    this.password.setValue('');
+  }
+
+  clearMajor(): void {
+    this.major.setValue('');
+  }
+
+  clearSport(): void {
+    this.sport.setValue('');
+  }
+
+  clearName(): void {
+    this.name.setValue('');
+  }
+
+  clearGender(): void {
+    this.gender.setValue('');
+  }
+
+  clearRelationship(): void {
+    this.relationship.setValue('');
+  }
   onSubmit(): void {
     // TODO: wire up to login request
     console.log(this.signupForm.value);
