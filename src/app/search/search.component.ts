77ede11b39902e1e13f030ba0b17134b8d8e53a0
Chase Olsen <skalolsen@gmail.com>
2020-12-31 20:21:53 -0500

    needs help

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,6 +9,7 @@ Component({
   styleUrls: ['./search.component.scss']
 })
 
+// Start
 // Need myVariable value to be filled by a users selection. 
 // Then a function to display the appropriate array.
 enum SearchOptions {
@@ -29,52 +30,53 @@ enum SearchOptions {
 }
 
 if myVariable === SearchOptions.College_Connection {
-  let alumni = ['Applied Science & Engineering', 'Architecture, Landscape & Design', 'Arts & Science', 'Continuing Studies', 'Dentistry', 'Education', 'Information', 'Kinesiology & Physical Education','Law','Management','Medicine', 'Music','Nursing','Pharmacy','Public Health','Social Work','Innis College','New College','St.Micheals College','Trinity College','University College','U Of T Mississauga Campus','U Of T Scarborough','Victoria College','Woodsworth College' ]
+  let specifics = ['Applied Science & Engineering', 'Architecture, Landscape & Design', 'Arts & Science', 'Continuing Studies', 'Dentistry', 'Education', 'Information', 'Kinesiology & Physical Education','Law','Management','Medicine', 'Music','Nursing','Pharmacy','Public Health','Social Work','Innis College','New College','St.Micheals College','Trinity College','University College','U Of T Mississauga Campus','U Of T Scarborough','Victoria College','Woodsworth College' ]
 }
 if myVariable === SearchOptions.Blues_Clubs {
-let blue = ['Academic','Arts','Athletics & Recreation','Community Service','Culture & Identities','Environment & Sustainability','Hobby & Leisure','Global Interests','Leadership','Media','Other','Politics','Social Justive & Advocay','Social','Spirituality & Faith Communities','Student Governments, Unions & Councils','Work & Career Development']
+let specifics = ['Academic','Arts','Athletics & Recreation','Community Service','Culture & Identities','Environment & Sustainability','Hobby & Leisure','Global Interests','Leadership','Media','Other','Politics','Social Justive & Advocay','Social','Spirituality & Faith Communities','Student Governments, Unions & Councils','Work & Career Development']
 }
 if myVariable === SearchOptions.COURSE_A_E {
-  let a_e = ['Academic Bridging Program','Acturial Science', 'American Studies', 'Anatomy',' Anthropology,','Archaeology','Architecture & Visual Studies','Art History','Astronomy & Astrophysics','Biochemistry','Biology','Candian Institutes For Theoretical Astrophysics','Cell & Systems Biology','Cinema Studies Institute', 'Computer Science','Contemporary Asian Studies','Criminology & Sociolegal Studies','Criminology & Sociolegal Studies','Diaspora & Transnational Studies','Drama', 'Theatre & Performance Studies','Earth Sciences','East Asian Studies','Ecology & Evolutionary Studies','Earth Sciences','East Asian Studies','Ecology & Evolutionary Biology','Economics','Environment(School of the)','Estonian','Ethics','European Studies']
+  let specifics = ['Academic Bridging Program','Acturial Science', 'American Studies', 'Anatomy',' Anthropology,','Archaeology','Architecture & Visual Studies','Art History','Astronomy & Astrophysics','Biochemistry','Biology','Candian Institutes For Theoretical Astrophysics','Cell & Systems Biology','Cinema Studies Institute', 'Computer Science','Contemporary Asian Studies','Criminology & Sociolegal Studies','Criminology & Sociolegal Studies','Diaspora & Transnational Studies','Drama', 'Theatre & Performance Studies','Earth Sciences','East Asian Studies','Ecology & Evolutionary Studies','Earth Sciences','East Asian Studies','Ecology & Evolutionary Biology','Economics','Environment(School of the)','Estonian','Ethics','European Studies']
 }
   if myVariable === SearchOptions.COURSE_F_M {
-    let f_m = ['Finnish','First-Year Foundations','Forest Conservation & Forest Biomaterials Science','French','Geography & Planning','German','History','History & Philosophy & Technology','Human Biology','Hungarian','Immunology','Impact Culture','Indigenous Studies','Industrial Relations & Resources (Centre For)','Innis College','Italian','Centre For Jewish Studies','Laboratory Medicine & Pathobiology','Latin American Studies','Life Sciences','Linguistics','Material Science','Mathematics','Molecular Genetics & Microbiology','Munk One','Music']
+    let specifics = ['Finnish','First-Year Foundations','Forest Conservation & Forest Biomaterials Science','French','Geography & Planning','German','History','History & Philosophy & Technology','Human Biology','Hungarian','Immunology','Impact Culture','Indigenous Studies','Industrial Relations & Resources (Centre For)','Innis College','Italian','Centre For Jewish Studies','Laboratory Medicine & Pathobiology','Latin American Studies','Life Sciences','Linguistics','Material Science','Mathematics','Molecular Genetics & Microbiology','Munk One','Music']
   }
     if myVariable === SearchOptions.COURSE_N_Z{
-    let n_z = ['Near & Middle Eastern Civilizations','New College','Nutritional Sciences','Peace, Conflict & Justice','Pharmacology & Toxicology','Philosophy','Physiology','Pysics','Planetary Science','Political Science','Portuguese','Psychology','Public Policy','Religion','Rotman Commerce','St.Michaels College','Sexual Diversity Studies','Slavic Languages & Literature','Sociology','South Asian Studies','Spanish','Statistical Sciences','Trinity College','University College','Victoria College','Women & Gender Studies','Woodsworth College','Yiddish Studies']
+    let specifics = ['Near & Middle Eastern Civilizations','New College','Nutritional Sciences','Peace, Conflict & Justice','Pharmacology & Toxicology','Philosophy','Physiology','Pysics','Planetary Science','Political Science','Portuguese','Psychology','Public Policy','Religion','Rotman Commerce','St.Michaels College','Sexual Diversity Studies','Slavic Languages & Literature','Sociology','South Asian Studies','Spanish','Statistical Sciences','Trinity College','University College','Victoria College','Women & Gender Studies','Woodsworth College','Yiddish Studies']
   }
 
   if myVariable === SearchOptions.ENTREP_IDEA {
-    let entrep =['Arts','Contracting','Enviromental & Conservation','Health Care','Money Managing','Non-Profit','Science & Technology','Sports','Travel','Unique Ideas']
+    let specifics =['Arts','Contracting','Enviromental & Conservation','Health Care','Money Managing','Non-Profit','Science & Technology','Sports','Travel','Unique Ideas']
   }
 
   if myVariable === SearchOptions.HELP_WANT {
-    let help = ['Cleaning','Cooking & Grociers','Designing','Electronic Based','Moving Furniture','Transportation','Walking Home']
+    let specifics = ['Cleaning','Cooking & Grociers','Designing','Electronic Based','Moving Furniture','Transportation','Walking Home']
   }
   if myVariable === SearchOptions.HOUSING{
-  let housing = ['Buy & Sell','Searching For Residence','Questions & Advice','Roomate Wanted','Subleting']
+  let specifics = ['Buy & Sell','Searching For Residence','Questions & Advice','Roomate Wanted','Subleting']
   }
   if myVariable === SearchOptions.MISCELL_BUY_SELL{
-    let buy = ['Looking To Buy/Sell','Miscellaneous']
+    let specifics = ['Looking To Buy/Sell','Miscellaneous']
   }
   if myVariable === SearchOptions.SCHOOL_WORK{
-    let school = ['Study Groups','Tutors','Buy/Sell School Supplies','Instructor Review','Question & Answer']
+    let specifics = ['Study Groups','Tutors','Buy/Sell School Supplies','Instructor Review','Question & Answer']
   }
   if myVariable === SearchOptions.SPIRITUALITY{
-    let spirit = ['Church Times & Locations','Faith Based Activities','Fundraisers','Need Support','Prayer Groups','Prayer Requests']
+    let specifics = ['Church Times & Locations','Faith Based Activities','Fundraisers','Need Support','Prayer Groups','Prayer Requests']
   }
   if myVariable === SearchOptions.U_OF_T_QS{
-    let questions = ['Class Registration Advice','Student Discounts','Open Questions']
+    let specifics = ['Class Registration Advice','Student Discounts','Open Questions']
   }
   if myVariable === SearchOptions.UPCOMING_EVENTS{
-    let upcoming = ['Arts & Science','Club Sports','Concerts','Drop In Games','Festivals','Fraternities & Sororities','Fundraisers','Intramural Sports','Miscellaneous','Parties & Clubs','Varsity Sports','Video & Board Games']
+    let specifics = ['Arts & Science','Club Sports','Concerts','Drop In Games','Festivals','Fraternities & Sororities','Fundraisers','Intramural Sports','Miscellaneous','Parties & Clubs','Varsity Sports','Video & Board Games']
   }
-
+// specificFunction records which value has been clikced. 
+// Then displayFunction switches which specific array is displayed. 
   function displayFunction(){
     var c = document.getElementById("myVariable").value;
     // display appropriate array in other display box on screen.
   }
-  // Fix
+  // End
 
 document.getElementById("").onclick = function() {
   // display appropriate array
