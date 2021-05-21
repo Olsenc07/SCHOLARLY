558cd23c75ac5b68a36ad5f716848a01ed363180
Mike LaFreniere <mlafreniere17@hotmail.com>
2021-05-21 06:37:37 -0600

    format code with prettier and add follow/unfollow text

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {MatBottomSheet} from '@angular/material/bottom-sheet';
-import {ProfileBottomSheetComponent} from './bottom-sheet/bottom-sheet.component';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { ProfileBottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
 
 @Component({
   selector: 'app-profile',
@@ -9,10 +9,15 @@ import {ProfileBottomSheetComponent} from './bottom-sheet/bottom-sheet.component
 })
 export class ProfileComponent {
   showFiller = false;
+  // TODO: initial following value would need to be loaded from database - for now, always start with false
+  following = false;
   constructor(private _BOTTOMSHEET: MatBottomSheet) {}
 
   openBottomSheet(): void {
     this._BOTTOMSHEET.open(ProfileBottomSheetComponent);
   }
-}
 
+  followClicked(): void {
+    this.following = !this.following;
+  }
+}
