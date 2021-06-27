5d5c0c5cbc286c104069f976df2663315d615778
Chase Olsen <skalolsen@gmail.com>
2021-06-26 22:54:27 -0400

    annoyed nede a break

--- src/app/reusable-card/reusable-card.component.ts
+++ src/app/reusable-card/reusable-card.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { AttendanceComponent } from '../main-pages/main-pages.component';
 
+
 @Component({
     selector: 'app-card',
     templateUrl: './reusable-card.component.html',
@@ -11,6 +12,9 @@ export class ReusableCardComponent {
     openAttendanceSheet(): void {
         this.bottomSheet.open(AttendanceComponent);
     }
+
+
+
 
     panelOpenState = false;
     constructor(private bottomSheet: MatBottomSheet) { }
