76133e91264617d3431d20b70eccdb1e22af46f2
Chase Olsen <skalolsen@gmail.com>
2021-07-05 18:24:18 -0400

    post page is screwed up

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -17,7 +17,7 @@ import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material
 import { MatChipInputEvent } from '@angular/material/chips';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+import { SearchListService } from '../services/search.service';
 
 
 const moment = _rollupMoment || _moment;
@@ -33,40 +33,10 @@ export const MY_FORMATS = {
   },
 };
 
-
-enum SearchValues {
-  COLLEGE_CONNECTION = 'college-connection',
-  BLUE_CLUBS = 'blue-clubs',
-  COURSE_A_E = 'course-a-e',
-  COURSE_F_M = 'course-f-m',
-  COURSE_N_Z = 'course-n-z',
-  ENTREP_IDEA = 'entrep',
-  HELP_WANTED = 'help',
-  HOUSING = 'housing',
-  MISCELL_BUY_SELL = 'buy-sell',
-  SCHOOL_WORK = 'school-work',
-  SPIRITUALITY = 'spiritituality',
-  U_OF_T_QS = 'questions',
-  UPCOMING_EVENTS = 'upcoming-events',
-}
 interface SearchOption {
   value: string;
   name: string;
 }
-
-
-
-enum BooleanOptions {
-  Can_Drive = 'able-to-drive',
-  Payement_Service = 'pay-service',
-  Formal_Event = 'formal-event',
-  Relaxed_Event = 'relaxed-event',
-  Male = 'male',
-  Female = 'female',
-  All = 'all',
-}
-
-
 
 @Component({
   selector: 'app-post-page',
@@ -84,6 +54,10 @@ enum BooleanOptions {
 })
 
 export class PostPageComponent implements OnInit {
+  public selectedOption: string;
+  public specificOptions: string[];
+  searchOptions: SearchOption[];
+
   Title = '';
 
   url: string;
@@ -115,20 +89,17 @@ export class PostPageComponent implements OnInit {
 
   isLinear = false;
   postTitle: FormControl = new FormControl('');
-  postTitleM: FormControl = new FormControl('');
   postUpload: FormControl = new FormControl('');
   postLocation: FormControl = new FormControl('');
   postLocationM: FormControl = new FormControl('');
   locationEvent: FormControl = new FormControl('');
   time: FormControl = new FormControl('');
-  timeM: FormControl = new FormControl('');
+
 
   value: FormControl = new FormControl('');
-  description: FormControl = new FormControl('');
+  postDescription: FormControl = new FormControl('');
   date: FormControl = new FormControl(moment());
-  dateM: FormControl = new FormControl(moment());
   upload: FormControl = new FormControl('');
-  uploadM: FormControl = new FormControl('');
   driver: FormControl = new FormControl('');
   paymentService: FormControl = new FormControl('');
   formalEvent: FormControl = new FormControl('');
@@ -136,6 +107,43 @@ export class PostPageComponent implements OnInit {
   male: FormControl = new FormControl('');
   female: FormControl = new FormControl('');
   all: FormControl = new FormControl('');
+
+  location: FormControl = new FormControl('');
+  search: FormControl = new FormControl('');
+
+  firstFormGroup = new FormGroup({
+    date: this.date,
+    time: this.time,
+    locationEvent: this.locationEvent,
+  });
+  secondFormGroup = new FormGroup({
+    male: this.male,
+    female: this.female,
+    all: this.all,
+  });
+  thirdFormGroup = new FormGroup({
+    driver: this.driver,
+    paymentService: this.paymentService,
+  });
+  fourthFormGroup = new FormGroup({
+    formalEvent: this.formalEvent,
+    relaxedEvent: this.relaxedEvent,
+  });
+  postForm = new FormGroup({
+    // Desktop
+    postTitle: this.postTitle,
+    postDescription: this.postDescription,
+    postUpload: this.postUpload,
+    firstFormGroup: this.firstFormGroup,
+    secondFormGroup: this.secondFormGroup,
+    thirdFormGroup: this.thirdFormGroup,
+    fourthFormGroup: this.fourthFormGroup,
+    postLocation: this.postLocation,
+    friendCtrl: this.friendCtrl,
+    upload: this.upload,
+  });
+
+  // Mobile
   driverM: FormControl = new FormControl('');
   paymentServiceM: FormControl = new FormControl('');
   formalEventM: FormControl = new FormControl('');
@@ -143,71 +151,47 @@ export class PostPageComponent implements OnInit {
   maleM: FormControl = new FormControl('');
   femaleM: FormControl = new FormControl('');
   allM: FormControl = new FormControl('');
+  uploadM: FormControl = new FormControl('');
+  postTitleM: FormControl = new FormControl('');
+  dateM: FormControl = new FormControl('');
+  locationEventM: FormControl = new FormControl('');
+  timeM: FormControl = new FormControl('');
 
-  location: FormControl = new FormControl('');
-  search: FormControl = new FormControl('');
 
-  firstFormGroup = new FormGroup({
-
+  firstFormGroupM = new FormGroup({
+    dateM: this.dateM,
+    timeM: this.timeM,
+    locationEventM: this.locationEventM,
   });
-  secondFormGroup = new FormGroup({
-
+  secondFormGroupM = new FormGroup({
+    maleM: this.maleM,
+    femaleM: this.femaleM,
+    allM: this.allM,
   });
-  thirdFormGroup = new FormGroup({
-
+  thirdFormGroupM = new FormGroup({
+    driverM: this.driverM,
+    paymentServiceM: this.paymentServiceM,
   });
-  fourthFormGroup = new FormGroup({
-
+  fourthFormGroupM = new FormGroup({
+    formalEventM: this.formalEventM,
+    relaxedEventM: this.relaxedEventM,
   });
-  postForm = new FormGroup({
-    // Desktop
-    postTitle: this.postTitle,
-    description: this.description,
-    postUpload: this.postUpload,
-    friendCtrl: this.friendCtrl,
-    postLocation: this.postLocation,
-    locationEvent: this.locationEvent,
-    upload: this.upload,
-    driver: this.driver,
-    paymentService: this.paymentService,
-    formalEvent: this.formalEvent,
-    relaxedEvent: this.relaxedEvent,
-    male: this.male,
-    female: this.female,
-    all: this.all,
-    date: this.date,
-    time: this.time,
-    // Mobile
+
+
+  postFormM = new FormGroup({
     postTitleM: this.postTitleM,
     postLocationM: this.postLocationM,
     location: this.location,
     search: this.search,
-
     value: this.value,
     uploadM: this.uploadM,
     dateM: this.dateM,
     timeM: this.timeM,
-  });
+    friendCtrlM: this.friendCtrlM,
 
+  })
 
-  public searchOptions: SearchOption[] = [
-    { name: 'Blues Clubs', value: SearchValues.BLUE_CLUBS },
-    { name: 'Buy & Sell', value: SearchValues.MISCELL_BUY_SELL },
-    { name: 'College Connection', value: SearchValues.COLLEGE_CONNECTION },
-    { name: 'Courses A-E', value: SearchValues.COURSE_A_E },
-    { name: 'Courses F-M', value: SearchValues.COURSE_F_M },
-    { name: 'Courses N-Z', value: SearchValues.COURSE_N_Z },
-    { name: 'Entrepreneurial Opportunities', value: SearchValues.ENTREP_IDEA },
-    { name: 'Helping Hand', value: SearchValues.HELP_WANTED },
-    { name: 'Housing', value: SearchValues.HOUSING },
-    { name: 'School Work', value: SearchValues.SCHOOL_WORK },
-    { name: 'Spirituality', value: SearchValues.SPIRITUALITY },
-    { name: 'Questions', value: SearchValues.U_OF_T_QS },
-    { name: 'Upcoming Events', value: SearchValues.UPCOMING_EVENTS },
-  ];
-  public selectedOption: string;
-  public specificOptions: string[];
-  constructor(public dialog: MatDialog, private FORMBuilder: FormBuilder) {
+  constructor(public dialog: MatDialog, private FORMBuilder: FormBuilder, public searchListService: SearchListService) {
     // Desktop tag friends
     this.filteredFriends = this.friendCtrl.valueChanges.pipe(
       map((friend: string | null) => friend ? this._filter(friend) : this.allFriends.slice()));
@@ -237,7 +221,7 @@ export class PostPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    this.searchOptions = this.searchListService.getSearchOptions();
 
     this.firstFormGroup = this.FORMBuilder.group({
       firstCtrl: ['']
@@ -251,6 +235,24 @@ export class PostPageComponent implements OnInit {
     this.fourthFormGroup = this.FORMBuilder.group({
       fourthCtrl: ['']
     });
+
+
+    this.firstFormGroupM = this.FORMBuilder.group({
+      firstCtrlM: ['']
+    });
+    this.secondFormGroupM = this.FORMBuilder.group({
+      secondCtrlM: ['']
+    });
+    this.thirdFormGroupM = this.FORMBuilder.group({
+      thirdCtrlM: ['']
+    });
+    this.fourthFormGroupM = this.FORMBuilder.group({
+      fourthCtrlM: ['']
+    });
+  }
+  onSearchSelection(value): void {
+    console.log(value);
+    this.specificOptions = this.searchListService.onSearchSelection(value);
   }
   // Desktop tag friends
   add(event: MatChipInputEvent): void {
@@ -305,238 +307,7 @@ export class PostPageComponent implements OnInit {
     const filterValueM = valueM.toLowerCase();
     return this.allFriendsM.filter(friendM => friendM.toLowerCase().indexOf(filterValueM) === 0);
   }
-  // First step at ability to uplaod img/file attempt
-  OnFileSelected(event: Event): void { }
-  onSearchSelection(value: string): void {
-    switch (value) {
-      case SearchValues.COLLEGE_CONNECTION:
-        this.specificOptions = [
-          'Applied Science & Engineering',
-          'Architecture, Landscape & Design',
-          'Arts & Science',
-          'Continuing Studies',
-          'Dentistry',
-          'Education',
-          'Information',
-          'Kinesiology & Physical Education',
-          'Law',
-          'Management',
-          'Medicine',
-          'Music',
-          'Nursing',
-          'Pharmacy',
-          'Public Health',
-          'Social Work',
-          'Innis College',
-          'New College',
-          'St.Micheals College',
-          'Trinity College',
-          'University College',
-          'U Of T Mississauga Campus',
-          'U Of T Scarborough',
-          'Victoria College',
-          'Woodsworth College',
-        ];
-        break;
-      case SearchValues.BLUE_CLUBS:
-        this.specificOptions = [
-          'Academic',
-          'Arts',
-          'Athletics & Recreation',
-          'Community Service',
-          'Culture & Identities',
-          'Environment & Sustainability',
-          'Hobby & Leisure',
-          'Global Interests',
-          'Leadership',
-          'Media',
-          'Other',
-          'Politics',
-          'Social Justice & Advocacy',
-          'Social',
-          'Spirituality & Faith Communities',
-          'Student Governments, Unions & Councils',
-          'Work & Career Development',
-        ];
-        break;
-      case SearchValues.COURSE_A_E:
-        this.specificOptions = [
-          'Academic Bridging Program',
-          'Acturial Science',
-          'American Studies',
-          'Anatomy',
-          'Anthropology,',
-          'Archaeology',
-          'Architecture & Visual Studies',
-          'Art History',
-          'Astronomy & Astrophysics',
-          'Biochemistry',
-          'Biology',
-          'Theoretical Astrophysics',
-          'Cell & Systems Biology',
-          'Cinema Studies Institute',
-          'Computer Science',
-          'Contemporary Asian Studies',
-          'Criminology & Sociolegal Studies',
-          'Diaspora & Transnational Studies',
-          'Drama',
-          'Theatre & Performance Studies',
-          'Earth Sciences',
-          'East Asian Studies',
-          'Ecology & Evolutionary Studies',
-          'Economics',
-          'Environmental School',
-          'Estonian',
-          'Ethics',
-          'European Studies',
-        ];
-        break;
-      case SearchValues.COURSE_F_M:
-        this.specificOptions = [
-          'Finnish',
-          'First-Year Foundations',
-          'Forest Conservation & Forest Biomaterials Science',
-          'French',
-          'Geography & Planning',
-          'German',
-          'History',
-          'History & Philosophy & Technology',
-          'Human Biology',
-          'Hungarian',
-          'Immunology',
-          'Impact Culture',
-          'Indigenous Studies',
-          'Industrial Relations & Resources (Centre For)',
-          'Innis College',
-          'Italian',
-          'Centre For Jewish Studies',
-          'Laboratory Medicine & Pathobiology',
-          'Latin American Studies',
-          'Life Sciences',
-          'Linguistics',
-          'Material Science',
-          'Mathematics',
-          'Molecular Genetics & Microbiology',
-          'Munk One',
-          'Music',
-        ];
-        break;
-      case SearchValues.COURSE_N_Z:
-        this.specificOptions = [
-          'Near & Middle Eastern Civilizations',
-          'New College',
-          'Nutritional Sciences',
-          'Peace, Conflict & Justice',
-          'Pharmacology & Toxicology',
-          'Philosophy',
-          'Physiology',
-          'Physics',
-          'Planetary Science',
-          'Political Science',
-          'Portuguese',
-          'Psychology',
-          'Public Policy',
-          'Religion',
-          'Rotman Commerce',
-          'St.Michaels College',
-          'Sexual Diversity Studies',
-          'Slavic Languages & Literature',
-          'Sociology',
-          'South Asian Studies',
-          'Spanish',
-          'Statistical Sciences',
-          'Trinity College',
-          'University College',
-          'Victoria College',
-          'Women & Gender Studies',
-          'Woodsworth College',
-          'Yiddish Studies',
-        ];
-        break;
-      case SearchValues.ENTREP_IDEA:
-        this.specificOptions = [
-          'Arts',
-          'Contracting',
-          'Enviromental & Conservation',
-          'Health Care',
-          'Money Managing',
-          'Non-Profit',
-          'Science & Technology',
-          'Sports',
-          'Travel',
-          'Unique Ideas',
-        ];
-        break;
-      case SearchValues.HELP_WANTED:
-        this.specificOptions = [
-          'Cleaning',
-          'Cooking & Grociers',
-          'Designing',
-          'Electronic Based',
-          'Moving Furniture',
-          'Transportation',
-          'Walking Home',
-        ];
-        break;
-      case SearchValues.HOUSING:
-        this.specificOptions = [
-          'Buy & Sell',
-          'Searching For Residence',
-          'Questions & Advice',
-          'Roomate Wanted',
-          'Subleting',
-        ];
-        break;
-      case SearchValues.MISCELL_BUY_SELL:
-        this.specificOptions = ['Looking To Buy/Sell', 'Miscellaneous'];
-        break;
-      case SearchValues.SCHOOL_WORK:
-        this.specificOptions = [
-          'Study Groups',
-          'Tutors',
-          'Buy/Sell School Supplies',
-          'Instructor Review',
-          'Question & Answer',
-        ];
-        break;
-      case SearchValues.SPIRITUALITY:
-        this.specificOptions = [
-          'Church Times & Locations',
-          'Faith Based Activities',
-          'Fundraisers',
-          'Need Support',
-          'Prayer Groups',
-          'Prayer Requests',
-        ];
-        break;
-      case SearchValues.U_OF_T_QS:
-        this.specificOptions = [
-          'Class Registration Advice',
-          'Student Discounts',
-          'Open Questions',
-        ];
-        break;
-      case SearchValues.UPCOMING_EVENTS:
-        this.specificOptions = [
-          'Arts & Science',
-          'Club Sports',
-          'Concerts',
-          'Drop In Games',
-          'Festivals',
-          'Fraternities & Sororities',
-          'Fundraisers',
-          'Intramural Sports',
-          'Miscellaneous',
-          'Parties & Clubs',
-          'Varsity Sports',
-          'Video & Board Games',
-        ];
-        break;
 
-      default:
-        this.specificOptions = [];
-    }
-  }
 
 
 
@@ -548,7 +319,7 @@ export class PostPageComponent implements OnInit {
   }
   clearTitle(): void {
     this.postTitle.setValue('');
-    this.title.setValue('');
+    this.postTitleM.setValue('');
   }
 
 
@@ -560,9 +331,22 @@ export class PostPageComponent implements OnInit {
 
   onFormSubmit(): void {
     // TODO: wire up to post request
+    console.log(this.firstFormGroup.value);
+    console.log(this.secondFormGroup.value);
+    console.log(this.thirdFormGroup.value);
+    console.log(this.fourthFormGroup.value);
     console.log(this.postForm.value);
   }
 
+
+  onFormSubmitM(): void {
+    // TODO: wire up to post request
+    console.log(this.firstFormGroupM.value);
+    console.log(this.secondFormGroupM.value);
+    console.log(this.thirdFormGroupM.value);
+    console.log(this.fourthFormGroupM.value);
+    console.log(this.postFormM.value);
+  }
 
   changeTab(): void {
     this.selectedIndex = this.selectedIndex === 1 ? 0 : 1;
