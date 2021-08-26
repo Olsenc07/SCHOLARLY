16ee979c5b49fbf501d159595164652fe1ec8c00
Chase Olsen <skalolsen@gmail.com>
2021-08-25 21:14:35 -0400

    ok

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -24,7 +24,8 @@ export class StoreService {
   static Com = [
     'IMC200h1', 'Mat244h1', 'apm346h1', 'jpe395h1', 'phy152h1', 'phy231h1', 'hps301h1', 'hps371h1', 'hps202h1', 'mat223h1', 'mat235y1'
     , 'ggr101h1', 'hps260h1', 'mat224h1', 'phy256h1'];
-  static Pur = ['HPS120h1', 'mat246h1', 'mat334h1', 'phy224h1', 'jph441h1', 'mat301h1', 'mat315h1', 'phy324h1'];
+  static Pur = ['HPS120h1', 'mat246h1', 'mat334h1', 'phy224h1',
+    'jph441h1', 'mat301h1', 'mat315h1', 'phy324h1'];
 
   profile$$: ReplaySubject<Profile> = new ReplaySubject<Profile>(1);
   static profile$$: string[];
